f73bdae5afb67182e11d5afc5cad47331375cc40
Thomas Backman <serenity@exscape.org>
2015-01-14 19:59:19 +0100

    Support variables/assignment; e and pi as constants

--- excalc.py
+++ excalc.py
@@ -2,11 +2,10 @@
 
 import sys, readline
 from ply import lex, yacc
+import math
 
-# TODO: comments, single and multi-line
-# TODO: variables/assignment
+# TODO: comments, single and multi-line...?
 # TODO: pre-defined functions (sqrt, sin, cos, tan, exp and a few other common ones)
-# TODO: pre-defined *variables* (e, pi)
 # TODO: error handling (lexing errors, syntax errors)
 
 # Stuff used by the lexer
@@ -16,7 +15,8 @@ tokens = ('INT', 'FLOAT',
 		  'TIMES', 'DIVIDE',
 		  'EXPONENT',
 		  'LPAREN', 'RPAREN',
-		  'EQEQ')
+		  'ASSIGN', 'EQEQ',
+		  'IDENT')
 
 def t_FLOAT(t):
 	r'\d+ (?:\.\d*)? e [+-]? \d+ | \d+\.\d*'
@@ -28,6 +28,7 @@ def t_INT(t):
 	t.value = int(t.value)
 	return t
 
+t_IDENT = r'[A-Za-z_][A-Za-z0-9_]*'
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
@@ -36,6 +37,7 @@ t_EXPONENT = r'\^'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_EQEQ = r'=='
+t_ASSIGN = r'='
 
 t_ignore = "\t\r\n "
 
@@ -45,6 +47,7 @@ def t_error(t):
 # Stuff used by the parser
 
 precedence = (
+	  ("left", "ASSIGN"),
 	  ("left", "EQEQ"),
 	  ("left", "PLUS", "MINUS"),
 	  ("left", "TIMES", "DIVIDE"),
@@ -54,15 +57,6 @@ precedence = (
 
 def p_error(p):
 	print ('p_error:', p)
-
-
-#
-# Here's the grammar used:
-# exp -> exp (PLUS|MINUS|TIMES|DIVIDE|EXPONENT) exp
-# exp -> ( exp )
-# exp -> number
-# number -> INT | FLOAT
-#
 
 def p_exp_binop(p):
 	'''exp : exp PLUS exp
@@ -86,12 +80,26 @@ def p_exp_float(p):
 	'exp : FLOAT'
 	p[0] = ("float", p[1])
 
+def p_exp_ident(p):
+	'exp : ident'
+	p[0] = p[1]
+
 def p_exp_uminus(p):
 	'exp : MINUS exp %prec UMINUS'
 	# Matches -exp and uses precedence UMINUS instead of the usual MINUS
 	p[0] = ("uminus", p[2])
 
+def p_ident(p):
+	'ident : IDENT'
+	p[0] = ("ident", p[1])
+
+def p_assign(p):
+	'exp : ident ASSIGN exp'
+	p[0] = ("assign", p[1], p[3])
+
 # End of parser definitions
+
+__state = {'e': math.e, 'pi': math.pi}
 
 def evaluate(expr):
 	""" Evaluates an expression (as a string) and lexes/parses it, then returns the result"""
@@ -131,14 +139,33 @@ def evaluate_tree(tree):
 		return tree[1]
 	elif kind == "uminus":
 		return -evaluate_tree(tree[1])
+	elif kind == "ident":
+		name = tree[1]
+		try:
+			return __state[name]
+		except KeyError:
+			raise KeyError('unknown variable name "{}"'.format(name))
+	elif kind == "assign":
+		name = tree[1][1]
+		val = tree[2]
+		__state[name] = evaluate_tree(val)
+
+		return __state[name]
 
 	print("ERROR: reached end of evaluate_tree for tree", tree)
 	sys.exit(1)
 
 if __name__ == '__main__':
 	while True:
-		input_str = input('>>> ')
-		if input_str == "":
+		try:
+			input_str = input('>>> ')
+			try:
+				result = evaluate(input_str)
+				print (result)
+			except KeyError as e:
+				print ("Error: {}".format(str(e)[1:-1]))
+			except OverflowError:
+				print ("Overflow: result is out of range")
+		except (KeyboardInterrupt, EOFError):
+			print("")
 			sys.exit(0)
-		result = evaluate(input_str)
-		print (result)
